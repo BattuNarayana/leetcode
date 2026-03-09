7b328a6c4b0991c19fd5f5e7af4e100d429e013f
BattuNarayana <162019169+BattuNarayana@users.noreply.github.com>
2026-03-09 11:05:40 +0530

    Time: 13 ms (99.81%), Space: 41.9 MB (85.51%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -2,17 +2,17 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {            // since the order is like T T T T T T T F F F F F....
-        int low = 1, high = n;                     // So , we can observe that after a certain point all are F...
+    public int firstBadVersion(int n) {            // since the order is like F F F F T T T T T T....
+        int low = 1, high = n;                     // So , we can observe that after a certain point all are T...
         while(low<high){                           // So we can apply binary search
             int mid = low + (high - low)/2;
-            if(isBadVersion(mid)){                 // if the current standing point is F, then it might be our answer or it can be in the LEFT
+            if(isBadVersion(mid)){                 // if the current standing point is T, then it might be our answer or it can be in the LEFT
                 high = mid;
             }
-            else{                                  // if the current standing point is T, then we can discard the left half as all are T only
+            else{                                  // if the current standing point is F, then we can discard the left half as all are T only
                 low = mid + 1;                     // and search space reduced to low= mid + 1
             }
         }
-        return high;                               // when low crosses high....its for sure that high will be pointing to the first F
+        return high;                               // when low crosses high....its for sure that high will be pointing to the first T
     }
 }
