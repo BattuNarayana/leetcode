ebe6a93cf8c961d83075a95388d042749a1a25cd
BattuNarayana <162019169+BattuNarayana@users.noreply.github.com>
2026-04-28 14:48:42 +0530

    Time: 0 ms (100%), Space: 43.6 MB (88.19%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,17 +4,18 @@ class Solution {
         int j = n - 1;      // last element in nums2
         int k = m + n - 1;  // last position in nums1
 
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        while (j >= 0) {
+            if (i >=0 && nums1[i]>=nums2[j]) {
+                nums1[k] = nums1[i--];
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[k] = nums2[j--];
             }
+            k--;
         }
 
-        // Fill remaining nums2 elements if any
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
+        // // Fill remaining nums2 elements if any
+        // while (j >= 0) {
+        //     nums1[k--] = nums2[j--];
+        // }
     }
 }
