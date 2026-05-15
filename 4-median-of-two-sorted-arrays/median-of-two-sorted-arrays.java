b472727492e915ce79e66b11ddf18f2bb8b398b0
BattuNarayana <162019169+BattuNarayana@users.noreply.github.com>
2026-05-15 20:07:22 +0530

    Time: 1 ms (100.00%) | Memory: 49.1 MB (15.26%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,47 +1,25 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
         int total = n1 + n2;
-        int i=0,j=0, count = 0,curr=0,prev=0;
-        int req = total%2;
-        while(i<n1 && j<n2){
-            prev = curr;
-            if(nums1[i]<=nums2[j]){
-                curr = nums1[i];
-                i++;
+        int leftSize = (total + 1)/2;
+        int low = 0, high = n1;
+        while(low<=high){
+            int cut1 = low + (high - low)/2;
+            int cut2 = leftSize - cut1;
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 -1];
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+            if(l1<=r2 && l2<=r1){
+                if(total%2==1) return Math.max(l1, l2);
+                return (Math.max(l1, l2) + Math.min(r1, r2))/2.0;
             }
-            else{
-                curr = nums2[j];
-                j++;
-            }
-            if(count == total/2){
-                if(req == 1) return 1.0*curr;
-                else{
-                    return (curr + prev)/2.0;
-                }
-            }
-            count++;
+            else if(l1>r2) high = cut1 - 1;
+            else low = cut1 + 1;
         }
-        while(i<n1){
-            prev = curr;
-            curr = nums1[i];
-            if(count == total/2){
-                if(req == 1) return 1.0*curr;
-                return (curr + prev)/2.0;
-            }
-            i++;
-            count++;
-        }
-        while(j<n2){
-            prev = curr;
-            curr = nums2[j];
-            if(count == total/2){
-                if(req == 1) return 1.0*curr;
-                return (curr + prev)/2.0;
-            }
-            j++;
-            count++;
-        }
-        return 0;
+        return 0.0;
     }
 }
