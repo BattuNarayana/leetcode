e00d1b28342d151332f1dea21ef1e0024c611ede
BattuNarayana <162019169+BattuNarayana@users.noreply.github.com>
2026-05-25 17:00:39 +0530

    Time: 4 ms (57.12%), Space: 44.3 MB (21.8%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Pair{
+    TreeNode node;
+    int row;
+    int col;
+    Pair(TreeNode node, int row, int col){
+        this.node = node;
+        this.row = row;
+        this.col = col;
+    }
+ }
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0, 0));
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            TreeNode node = curr.node;
+            int row = curr.row;
+            int col = curr.col;
+            map.putIfAbsent(col, new TreeMap<>());
+            map.get(col).putIfAbsent(row, new PriorityQueue<>());
+            map.get(col).get(row).offer(node.val);
+            if(node.left!=null) q.offer(new Pair(node.left, row+1,col - 1));
+            if(node.right!=null) q.offer(new Pair(node.right,row+1, col + 1));
+        }
+        for(TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()){
+            List<Integer> list = new ArrayList<>();
+            for(PriorityQueue<Integer> pq : ys.values()){
+                while(!pq.isEmpty()){
+                    list.add(pq.poll());
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
