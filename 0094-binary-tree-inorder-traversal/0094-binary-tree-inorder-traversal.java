bb3a38f52104713c6b8f1a055f519bd233ea4fd7
BattuNarayana <162019169+BattuNarayana@users.noreply.github.com>
2026-05-28 21:29:24 +0530

    Time: 0 ms (100%), Space: 43.5 MB (14.31%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,11 +16,33 @@
 class Solution {
     List<Integer> ans = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return new ArrayList<>();
 
-        if(root.left!=null) inorderTraversal(root.left);
-        ans.add(root.val);
-        if(root.right!=null) inorderTraversal(root.right);
+        // MORRIS TRAVERSAL ( WITHOUT STACK & RECURSION) S.C = O(1)
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        TreeNode curr = root;
+        while(curr !=null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev = prev.right;
+                }
+                if(prev.right==null){
+                    prev.right = curr;
+                    // ans.add(curr.val) , since it is inorder traversal, we should not process the curr b/f left
+                    curr = curr.left;
+                }
+                else{
+                    prev.right = null;
+                    ans.add(curr.val); // curr is processed after the left BUT before RIGHT
+                    curr = curr.right;
+                }
+            }
+        }
         return ans;
     }
 }
