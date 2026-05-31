b4760ce668de8e9f665bb41e63fe23cc3dd67b9e
BattuNarayana <162019169+BattuNarayana@users.noreply.github.com>
2026-05-31 16:51:26 +0530

    Time: 3 ms (76.17%), Space: 47.6 MB (10.06%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -13,15 +13,39 @@
  *     }
  * }
  */
-class Solution {
-    Set<Integer> st = new HashSet<>();
-    public boolean findTarget(TreeNode root, int k) {
-        return pushAll(root, k);
+class BSTIterator{
+    Stack<TreeNode> st = new Stack<>();
+    boolean reverse;
+    BSTIterator(TreeNode root, boolean reverse){
+        this.reverse = reverse;
+        pushAll(root);
     }
-    public boolean pushAll(TreeNode root, int k){
-        if(root==null) return false;
-        if(st.contains(k - root.val)) return true;
-        st.add(root.val);
-        return pushAll(root.left, k) || pushAll(root.right, k);
+    public void pushAll(TreeNode node){
+        while(node!=null){
+            st.push(node);
+            if(reverse) node = node.right;
+            else node = node.left;
+        }
+    }
+    int next(){
+        TreeNode node = st.pop();
+        if(reverse) pushAll(node.left);
+        else pushAll(node.right);
+        return node.val;
+    }
+}
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        BSTIterator l = new BSTIterator(root, false);
+        BSTIterator r = new BSTIterator(root, true);
+        int i = l.next();
+        int j = r.next();
+        while(i<j){
+            int sum = i+j;
+            if(sum == k) return true;
+            if(sum<k) i = l.next();
+            else j = r.next();
+        }
+        return false;
     }
 }
